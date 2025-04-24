cad31264d7ef92a445c4e383dadea214426d2c94
lonelyneutrin0 <belagal1@msu.edu>
2025-04-23 22:09:28 -0400

    Hyperparameter Optimization

--- aqua_blue_hyperopt/__init__.py
+++ aqua_blue_hyperopt/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 __authors__ = [
     "Hrishikesh Belagali"
 ]
@@ -7,5 +7,4 @@ __author_emails__ = [
 ]
 __url__ = "https://github.com/Chicago-Club-Management-Company/aqua-blue-hyperopt"
 
-from . import foo as foo
-from . import bar as bar
+from . import hyper as hyper
